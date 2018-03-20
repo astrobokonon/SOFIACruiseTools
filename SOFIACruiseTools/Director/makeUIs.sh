1f3bce7d5f6fca1aada57f947eae832044f9357b
Jacob Vander Vliet <jvliet@sofia.usra.edu>
2018-03-20 09:39:45 -0700

    Cleared old, unneeded files

--- SOFIACruiseTools/Director/makeUIs.sh
+++ SOFIACruiseTools/Director/makeUIs.sh
@@ -2,6 +2,5 @@
 
 pyuic5 directorStartupDialog.ui -o directorStartupDialog.py
 pyuic5 FITSKeywordPanel.ui -o FITSKeywordPanel.py
-pyuic5 instrumentWarning.ui -o instrumentWarning.py
 pyuic5 SOFIACruiseDirectorPanel.ui -o SOFIACruiseDirectorPanel.py
 
