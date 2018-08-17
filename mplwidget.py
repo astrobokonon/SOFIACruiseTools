790927fc7b80b36f87b376451a0cd86972f53599
Jacob Vander Vliet <jvliet@sofia.usra.edu>
2018-08-16 17:03:21 -0700

    Missing plotting widget

---
+++ mplwidget.py
@@ -0,0 +1,27 @@
+from PyQt5 import QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
+from matplotlib.figure import Figure
+import matplotlib
+
+matplotlib.use('QT5Agg')
+
+
+class MplCanvas(Canvas):
+    def __init__(self):
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+        Canvas.__init__(self, self.fig)
+        Canvas.setSizePolicy(self,
+                             QtWidgets.QSizePolicy.Expanding,
+                             QtWidgets.QSizePolicy.Expanding)
+        Canvas.updateGeometry(self)
+
+
+class MplWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.canvas = MplCanvas()
+        #self.vbl = QtGui.QVBoxLayout()
+        self.vbl = QtWidgets.QVBoxLayout()
+        self.vbl.addWidget(self.canvas)
+        self.setLayout(self.vbl)
