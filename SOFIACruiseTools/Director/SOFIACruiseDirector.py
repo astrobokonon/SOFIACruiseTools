c6f0f9b9021d9faced6f39d9517b09fb1973bba1
Jacob Vander Vliet <jvliet@sofia.usra.edu>
2018-08-21 23:47:11 -0700

    Moved leg timer class to seperature module

--- SOFIACruiseTools/Director/SOFIACruiseDirector.py
+++ SOFIACruiseTools/Director/SOFIACruiseDirector.py
@@ -80,6 +80,7 @@ from SOFIACruiseTools.Director.directorLog import DirectorLogDialog
 from SOFIACruiseTools.Director.startupApp import StartupApp
 from SOFIACruiseTools.Director.FITSKeyWordDialog import FITSKeyWordDialog
 from SOFIACruiseTools.Director.fitsHeader import FITSHeader
+from SOFIACruiseTools.Director.legTimer import LegTimerObj
 
 #import SOFIACruiseTools.Director.flightMapDialog as fm
 
@@ -2060,106 +2061,106 @@ class SOFIACruiseDirectorApp(QtWidgets.QMainWindow, scdp.Ui_MainWindow):
 #                        self.headers.insert(i, require)
 #                self.fitskwText.setText('Custom')
 
-
-class LegTimerObj(object):
-    """
-    Class to hold the leg timer
-
-    Calculates elapsed time, remaining time for the leg,
-    as well as handles response to start, stop, and reset buttons
-    """
-
-    def __init__(self):
-        """
-        Initializes the leg timer
-        """
-        self.status = 'stopped'
-        self.duration = None
-        self.init_duration = None
-        self.remaining = datetime.timedelta()
-        self.elapsed = datetime.timedelta()
-        self.timer_start = None
-        self.flight_parsed = False
-
-    def minute_adjust(self, key):
-        """ Adds or subtracts one minute from timer """
-        adjustment = datetime.timedelta(minutes=1)
-        if key == 'add':
-            self.duration += adjustment
-        elif key == 'sub':
-            self.duration -= adjustment
-
-    def print_state(self):
-        """ Prints status of class variables to screen """
-        print('\nTimer Stats:')
-        print('\tStatus = ', self.status)
-        print('\tDuration = ', self.duration, ' (', type(self.duration), ')')
-        print('\tRemaining = ', self.remaining, ' (', type(self.remaining), ')')
-        print('\tElapsed = ', self.elapsed, ' (', type(self.elapsed), ')')
-
-    def control_timer(self, key):
-        """
-        Starts, stops, or resets the leg control
-        """
-        # Check if a flight plan has been successfully loaded
-        if not self.flight_parsed:
-            # Leg_dur_from_mis is only set if a flight plan
-            # is successfully parsed. If it is still an empty string,
-            # don't do anything
-            print('No flight plan loaded, cannot start leg timer')
-            return
-
-        if key == 'start':
-            # Start button is pushed
-            if self.status == 'running':
-                return
-            elif self.status == 'stopped':
-                self.status = 'running'
-                self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
-                self.duration = self.init_duration
-            else:
-                # Paused
-                self.status = 'running'
-                self.timer_start = (datetime.datetime.utcnow().replace(
-                                    microsecond=0) - self.elapsed)
-        elif key == 'stop':
-            # Stop button pushed
-            if self.status == 'running':
-                self.status = 'paused'
-        elif key == 'reset':
-            # Reset button pushed
-            self.status = 'running'
-            self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
-            self.duration = self.init_duration
-        else:
-            # Invalid key
-            print('Invalid key for leg control = {0:s}'.format(key))
-            return
-
-    def timer_string(self, mode):
-        """
-        Calculates elapsed and remaining time.
-
-        Returns the desired time format, specified by mode
-        (either 'remaining' or 'elapsed') in a nicely formatted
-        string.
-        """
-        now = datetime.datetime.utcnow().replace(microsecond=0)
-        try:
-            self.elapsed = now - self.timer_start
-            self.remaining = self.duration - self.elapsed
-        except TypeError:
-            print('\n\nTypeError in timer_string:')
-            print(type(self.elapsed), type(now), type(self.timer_start))
-            print(type(self.remaining), type(self.duration), type(self.elapsed))
-            self.print_state()
-            sys.exit()
-        if mode == 'remaining':
-            if self.remaining < datetime.timedelta(0):
-                return '-'+clock_string(-self.remaining)
-            return clock_string(self.remaining)
-        return clock_string(self.elapsed)
-
+#
+#class LegTimerObj(object):
+#    """
+#    Class to hold the leg timer
+#
+#    Calculates elapsed time, remaining time for the leg,
+#    as well as handles response to start, stop, and reset buttons
+#    """
+#
+#    def __init__(self):
+#        """
+#        Initializes the leg timer
+#        """
+#        self.status = 'stopped'
+#        self.duration = None
+#        self.init_duration = None
+#        self.remaining = datetime.timedelta()
+#        self.elapsed = datetime.timedelta()
+#        self.timer_start = None
+#        self.flight_parsed = False
+#
+#    def minute_adjust(self, key):
+#        """ Adds or subtracts one minute from timer """
+#        adjustment = datetime.timedelta(minutes=1)
+#        if key == 'add':
+#            self.duration += adjustment
+#        elif key == 'sub':
+#            self.duration -= adjustment
+#
+#    def print_state(self):
+#        """ Prints status of class variables to screen """
+#        print('\nTimer Stats:')
+#        print('\tStatus = ', self.status)
+#        print('\tDuration = ', self.duration, ' (', type(self.duration), ')')
+#        print('\tRemaining = ', self.remaining, ' (', type(self.remaining), ')')
+#        print('\tElapsed = ', self.elapsed, ' (', type(self.elapsed), ')')
+#
+#    def control_timer(self, key):
+#        """
+#        Starts, stops, or resets the leg control
+#        """
+#        # Check if a flight plan has been successfully loaded
+#        if not self.flight_parsed:
+#            # Leg_dur_from_mis is only set if a flight plan
+#            # is successfully parsed. If it is still an empty string,
+#            # don't do anything
+#            print('No flight plan loaded, cannot start leg timer')
+#            return
+#
+#        if key == 'start':
+#            # Start button is pushed
+#            if self.status == 'running':
+#                return
+#            elif self.status == 'stopped':
+#                self.status = 'running'
+#                self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
+#                self.duration = self.init_duration
+#            else:
+#                # Paused
+#                self.status = 'running'
+#                self.timer_start = (datetime.datetime.utcnow().replace(
+#                                    microsecond=0) - self.elapsed)
+#        elif key == 'stop':
+#            # Stop button pushed
+#            if self.status == 'running':
+#                self.status = 'paused'
+#        elif key == 'reset':
+#            # Reset button pushed
+#            self.status = 'running'
+#            self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
+#            self.duration = self.init_duration
+#        else:
+#            # Invalid key
+#            print('Invalid key for leg control = {0:s}'.format(key))
+#            return
+#
+#    def timer_string(self, mode):
+#        """
+#        Calculates elapsed and remaining time.
+#
+#        Returns the desired time format, specified by mode
+#        (either 'remaining' or 'elapsed') in a nicely formatted
+#        string.
+#        """
+#        now = datetime.datetime.utcnow().replace(microsecond=0)
+#        try:
+#            self.elapsed = now - self.timer_start
+#            self.remaining = self.duration - self.elapsed
+#        except TypeError:
+#            print('\n\nTypeError in timer_string:')
+#            print(type(self.elapsed), type(now), type(self.timer_start))
+#            print(type(self.remaining), type(self.duration), type(self.elapsed))
+#            self.print_state()
+#            sys.exit()
+#        if mode == 'remaining':
+#            if self.remaining < datetime.timedelta(0):
+#                return '-'+clock_string(-self.remaining)
+#            return clock_string(self.remaining)
+#        return clock_string(self.elapsed)
+#
 
 #class FlightMap(QtWidgets.QDialog, fm.Ui_Dialog):
 #    """Class for pop-out flight map"""
@@ -2187,22 +2188,22 @@ class LegTimerObj(object):
 #        y = x**2
 ##        self.figure.clear()
 ##        ax = self.figure.add_subplot(111)
-        self.flightMap.canvas.ax.plot(x, y)
-        self.flightMap.canvas.draw()
+#        self.flightMap.canvas.ax.plot(x, y)
+#        self.flightMap.canvas.draw()
+#
+#    def clear(self):
+#        """Clears the plot"""
+#        self.flightMap.canvas.ax.clear()
+#        self.flightMap.canvas.draw()
+#        #self.figure.clear()
 
-    def clear(self):
-        """Clears the plot"""
-        self.flightMap.canvas.ax.clear()
-        self.flightMap.canvas.draw()
-        #self.figure.clear()
 
-
-def clock_string(clock):
-    """ Formats timedelta into HH:MM:SS """
-    hours = clock.seconds//3600
-    minutes = clock.seconds//60 % 60
-    seconds = clock.seconds % 60
-    return '{0:02d}:{1:02d}:{2:02d}'.format(hours, minutes, seconds)
+#def clock_string(clock):
+#    """ Formats timedelta into HH:MM:SS """
+#    hours = clock.seconds//3600
+#    minutes = clock.seconds//60 % 60
+#    seconds = clock.seconds % 60
+#    return '{0:02d}:{1:02d}:{2:02d}'.format(hours, minutes, seconds)
 
 
 def total_sec_to_hms_str(obj):
---
+++ SOFIACruiseTools/Director/legTimer.py
@@ -0,0 +1,111 @@
+
+import datetime
+import sys
+
+
+class LegTimerObj(object):
+    """
+    Class to hold the leg timer
+
+    Calculates elapsed time, remaining time for the leg,
+    as well as handles response to start, stop, and reset buttons
+    """
+
+    def __init__(self):
+        """
+        Initializes the leg timer
+        """
+        self.status = 'stopped'
+        self.duration = None
+        self.init_duration = None
+        self.remaining = datetime.timedelta()
+        self.elapsed = datetime.timedelta()
+        self.timer_start = None
+        self.flight_parsed = False
+
+    def minute_adjust(self, key):
+        """ Adds or subtracts one minute from timer """
+        adjustment = datetime.timedelta(minutes=1)
+        if key == 'add':
+            self.duration += adjustment
+        elif key == 'sub':
+            self.duration -= adjustment
+
+    def print_state(self):
+        """ Prints status of class variables to screen """
+        print('\nTimer Stats:')
+        print('\tStatus = ', self.status)
+        print('\tDuration = ', self.duration, ' (', type(self.duration), ')')
+        print('\tRemaining = ', self.remaining, ' (', type(self.remaining), ')')
+        print('\tElapsed = ', self.elapsed, ' (', type(self.elapsed), ')')
+
+    def control_timer(self, key):
+        """
+        Starts, stops, or resets the leg control
+        """
+        # Check if a flight plan has been successfully loaded
+        if not self.flight_parsed:
+            # Leg_dur_from_mis is only set if a flight plan
+            # is successfully parsed. If it is still an empty string,
+            # don't do anything
+            print('No flight plan loaded, cannot start leg timer')
+            return
+
+        if key == 'start':
+            # Start button is pushed
+            if self.status == 'running':
+                return
+            elif self.status == 'stopped':
+                self.status = 'running'
+                self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
+                self.duration = self.init_duration
+            else:
+                # Paused
+                self.status = 'running'
+                self.timer_start = (datetime.datetime.utcnow().replace(
+                                    microsecond=0) - self.elapsed)
+        elif key == 'stop':
+            # Stop button pushed
+            if self.status == 'running':
+                self.status = 'paused'
+        elif key == 'reset':
+            # Reset button pushed
+            self.status = 'running'
+            self.timer_start = datetime.datetime.utcnow().replace(microsecond=0)
+            self.duration = self.init_duration
+        else:
+            # Invalid key
+            print('Invalid key for leg control = {0:s}'.format(key))
+            return
+
+    def timer_string(self, mode):
+        """
+        Calculates elapsed and remaining time.
+
+        Returns the desired time format, specified by mode
+        (either 'remaining' or 'elapsed') in a nicely formatted
+        string.
+        """
+        now = datetime.datetime.utcnow().replace(microsecond=0)
+        try:
+            self.elapsed = now - self.timer_start
+            self.remaining = self.duration - self.elapsed
+        except TypeError:
+            print('\n\nTypeError in timer_string:')
+            print(type(self.elapsed), type(now), type(self.timer_start))
+            print(type(self.remaining), type(self.duration), type(self.elapsed))
+            self.print_state()
+            sys.exit()
+        if mode == 'remaining':
+            if self.remaining < datetime.timedelta(0):
+                return '-'+clock_string(-self.remaining)
+            return clock_string(self.remaining)
+        return clock_string(self.elapsed)
+
+
+def clock_string(clock):
+    """ Formats timedelta into HH:MM:SS """
+    hours = clock.seconds//3600
+    minutes = clock.seconds//60 % 60
+    seconds = clock.seconds % 60
+    return '{0:02d}:{1:02d}:{2:02d}'.format(hours, minutes, seconds)
